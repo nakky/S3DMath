88481978180ac73a43312c695806edfc2083c60a
Naoto Nakamura <n_nakamura@jollystics.com>
2023-11-28 12:30:04 +0900

    Fix update bug.

--- src/Cluster.cpp
+++ src/Cluster.cpp
@@ -6,18 +6,21 @@ namespace S3DMath
     {
         target->mIsActiveInTree = target->isActive() && parentIsActive;
 
+        bool retval = false;
+
         bool needUpdate = parentUpdate || target->needUpdateState();
         if (target->isActive() && needUpdate)
         {
             target->updateState();
+            retval = true;
         }
-        bool retval = false;
+
         auto children = target->mNode.getChildList();
         for (auto ite = children.begin(); ite != children.end(); ite++)
         {
             Entity *next = (Entity *)(*ite)->getUserData();
             bool ret = tryUpdateInternal(next, needUpdate, target->isActiveInTree());
-            if (ret)
+            if (ret || retval)
                 retval = true;
         }
 
